11c942c91fefc8bda83f56ce02320beed5d24597
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-04-18 16:38:37 +0700

    add Laravel Service Provider

---
+++ Thumbnailer/ThumbnailerServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LiveCMS\Support\Thumbnailer;
+
+use LiveCMS\Support\Thumbnailer\Thumbnailer;
+use Illuminate\Support\ServiceProvider;
+
+class ThumbnailerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('thumbnailer', function () {
+            return new Thumbnailer;
+        });
+    }
+}
